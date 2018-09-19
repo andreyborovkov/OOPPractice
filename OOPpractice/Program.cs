1719b964c32a9a606f65d6071c6c579fffc93a0d
andreyborovkov <andrey_borovkov@yahoo.com>
2018-09-19 13:20:04 -0400

    initial commit

---
+++ OOPpractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dog woofy = new Dog();
+            //Dog spot = new Dog();
+            //Dog woofy = new Dog(7, 8.5, 33.2, 190.1, "Woofy");
+            //Dog spot = new Dog(15, 10.5, 100.9, 250.3, "Spot");
+
+            //Console.WriteLine("Woofie's hair length is: {0} ", woofy.HairLength);
+            //Console.WriteLine("Spot's hair length is: {0} ", spot.HairLength);
+
+            //woofy.HairLength = 4;
+            //spot.HairLength = 6;
+            //woofy.GetSpeed(7);
+            //spot.GetSpeed(11);
+            //Console.WriteLine("Woofy's hair length is: {0} ", woofy.HairLength);
+            //Console.WriteLine("Spot's hair length is: {0} ", spot.HairLength);
+            //Console.WriteLine("Woofiy is running: {0}", woofy.GetSpeed(7));
+
+            Superhero hulk = new Superhero("Bruce", 20, 3.5, false);
+            hulk.GetDetails();
+            hulk.GetHulk();
+            hulk.GetDetails();
+            hulk.GetBruce();
+            hulk.GetDetails();
+            hulk.GetHulk();
+            hulk.GetHulk();
+            hulk.GetDetails();
+        }
+    }
+}
---
+++ OOPpractice/Superhero.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPpractice
+{
+    class Superhero
+    {
+        //fields
+        private string name;
+        private int strength;
+        private double speed;
+        private bool isTransformed = false;
+
+        //properties
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Strength
+        {
+            get { return this.strength; }
+            set { this.strength = value; }
+        }
+
+        public double Speed
+        {
+            get { return this.speed; }
+            set { this.speed = value; }
+        }
+        public bool IsTransformed
+        {
+            get { return this.isTransformed; }
+            set { this.isTransformed = value; }
+        }
+
+        //Constructors
+        public Superhero()
+        {
+            //default
+        }
+
+        public Superhero(string name, int strength, double speed, bool isTransformed)
+        {
+            this.Name = name;
+            this.Strength = strength;
+            this.Speed = speed;
+            this.IsTransformed = isTransformed;
+        }
+
+        public Superhero(string name, int strength, double speed)
+        {
+            this.Name = name;
+            this.Strength = strength;
+            this.Speed = speed;
+        }
+
+        public string GetHulk()
+        {
+            if (isTransformed != true) { 
+            IsTransformed = true;
+            Name = "Hulk";
+            Strength = Strength * 200;
+            Speed = Speed * 100;
+        }
+
+            return Name;
+        }
+
+        public string GetBruce()
+        {
+            IsTransformed = false;
+            Name = "Bruce";
+            Strength = Strength / 200;
+            Speed = Speed / 100;
+
+
+            return Name;
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("My name is " + Name + ". I am " + Strength + "psi strong, " + Speed + "mph fast, and it's " + IsTransformed + " that I am transformed.");
+        }
+
+    }
+}
